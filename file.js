9e69d185b05d2c102a848c68a45118179ae5dce5
Souvik-R <souvikr212@gmail.com>
2025-09-08 00:29:01 +0530

    changes on file system

--- file.js
+++ file.js
@@ -1,19 +1,28 @@
 const fs = require("fs");
 
-fs.writeFileSync("./text.txt", "Hello Souvik");
+// fs.writeFileSync("./text.txt", "Hello Souvik");
 
 
-fs.writeFileSync("./doc.txt", "Doc to be create", (err) => {
-    if(err) return err;
-});
+// fs.writeFile("./doc.txt", "Doc to be create", (err) => {
+//     if(err) return err;
+// });
 
-const result2 = fs.readFileSync("./text.txt", "utf-8");
-console.log(result2);
+// const result2 = fs.readFileSync("./text.txt", "utf-8");
+// console.log(result2);
 
-fs.readFile("./doc.txt", "utf-8", (err, data) => {
-    if(err){
-        console.error(err);
-    } else {
-        console.log(data);
-    }
-})
+// fs.readFile("./doc.txt", "utf-8", (err, data) => {
+//     if(err){
+//         console.error(err);
+//     } else {
+//         console.log(data);
+//     }
+// });
+
+// ************************* 
+
+fs.appendFileSync("./text.txt", `${Date.now()} Hey There\n`);
+
+fs.copyFileSync("./text.txt", "./copy.txt")
+
+fs.unlinkSync("./copy.txt")
+
