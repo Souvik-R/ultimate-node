11e87e15d5d08936e192d5387deca63e75cf798b
Souvik-R <souvikr212@gmail.com>
2025-09-07 23:11:19 +0530

    changs

---
+++ file.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+fs.writeFileSync("./text.txt", "Hello Souvik");
+
+
+fs.writeFileSync("./doc.txt", "Doc to be create", (err) => {
+    if(err) return err;
+});
+
+const result2 = fs.readFileSync("./text.txt", "utf-8");
+console.log(result2);
+
+fs.readFile("./do.txt", "utf-8", (err, data) => {
+    if(err){
+        console.error(err);
+    } else {
+        console.log(data);
+    }
+})
